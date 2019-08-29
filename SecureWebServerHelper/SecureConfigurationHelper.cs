539def2adffb46182216679cccded01cca402f7b
Florian Wagner <florian.wagner@microsoft.com>
2019-08-29 09:59:34 +0900

    AB#8

--- SecureWebServerHelper/SecureConfigurationHelper.cs
+++ SecureWebServerHelper/SecureConfigurationHelper.cs
@@ -1,4 +1,27 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿/************************************************************************************************
+The MIT License (MIT)
+
+Copyright (c) Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+***********************************************************************************************/
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using System;
 using Microsoft.Azure.KeyVault;
@@ -7,34 +30,37 @@ using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using System.Security.Cryptography.X509Certificates;
 using System.Net;
+using System.Collections.Generic;
 
 namespace CSE.SecureWebServerHelper
 {
     public static class SecureConfigurationHelper
     {
-        private static KeyVaultClient keyVaultClient;
+        private static Dictionary<string, KeyVaultClient> keyVaultClients = new Dictionary<string, KeyVaultClient>();
         private static string envCertificateName = Environment.GetEnvironmentVariable("CertificateName");
         private static string envKeyVaultName = Environment.GetEnvironmentVariable("KeyVaultName");
         private static string envAzureConnectionString = Environment.GetEnvironmentVariable("AzureConnectionString");
+
 
         /// <summary>
         /// Adding KeyVault to App Configuration, all secrets in keyvault will be available as App configurations
         /// </summary>
         /// <param name="webHostBuilder">IWebHostBuilder</param>
         /// <param name="keyVaultName">Optional keyVaultName, otherwise will be retrieved by ENV 'KeyVaultName'</param>
+        /// <param name="azureConnectionString">Optional azureConnectionString, otherwise will be retrieved by ENV 'AzureConnectionString'</param>
         /// <returns>IWebHostBuilder instance</returns>
-        public static IWebHostBuilder ConfigurationFromKeyVault(this IWebHostBuilder webHostBuilder, string keyVaultName = null)
+        public static IWebHostBuilder ConfigurationFromKeyVault(this IWebHostBuilder webHostBuilder, string keyVaultName = null, string azureConnectionString = null)
         {
-            PrepareKeyVaultClient();
+            var keyVaultClient = PrepareKeyVaultClient(azureConnectionString);
             LoadValues(ref keyVaultName);
 
             webHostBuilder.
                 ConfigureAppConfiguration((context, config) =>
                 {
-
-                    if (context.HostingEnvironment.IsDevelopment() && envAzureConnectionString == null)
+                    // Warning message that if it is not working on development machine.
+                    if (context.HostingEnvironment.IsDevelopment() && envAzureConnectionString == null && azureConnectionString == null)
                     {
-                        Console.WriteLine("please specify env variable 'AzureConnectionString' with value 'RunAs=App;AppId={AppId};TenantId={TenantId};AppKey={ClientSecret}' or configure managed identity (if development infra is wihtin Azure).");
+                        Console.WriteLine("WARNING: In case of unexpected behavior/authentication errors please specify env variable 'AzureConnectionString' with value 'RunAs=App;AppId={AppId};TenantId={TenantId};AppKey={ClientSecret}' or configure managed identity (if development infra is wihtin Azure).");
                     }
 
                     config.AddAzureKeyVault(
@@ -47,14 +73,14 @@ namespace CSE.SecureWebServerHelper
         }
 
         /// <summary>
-        /// Adding Azure App Configuration service to App Configuration, all configurations stored in Azure App Configuration will be available as App configurations
+        /// Adding Azure App Configuration service to App Configuration, all configurations stored in Azure App Configuration will be available as App configurations.
+        /// The ideal case is that this connection string is stored in a Key Vault. Please use first .ConfigurationFromKeyVault() method to retrieve the connection string from there.
         /// </summary>
         /// <param name="webHostBuilder">IWebHostBuilder</param>
         /// <param name="azureAppConfigConnectionStringKey">The app configuration parameter to Azure App Configuration connection String (should be stored in KeyVault)</param>
         /// <returns>IWebHostBuilder instance</returns>
         public static IWebHostBuilder ConfigurationFromAzureAppConfig(this IWebHostBuilder webHostBuilder, string azureAppConfigConnectionStringKey = "ConnectionStrings:AppConfig")
         {
-            PrepareKeyVaultClient();
 
             webHostBuilder.
                 ConfigureAppConfiguration((context, config) =>
@@ -80,23 +106,23 @@ namespace CSE.SecureWebServerHelper
         /// <param name="webHostBuilder">IWebHostBuilder</param>
         /// <param name="keyVaultName">Optional keyVaultName, otherwise will be retrieved by ENV 'KeyVaultName'</param>
         /// <param name="certificateName">Optional certificateName, otherwise will be retrieved by ENV 'CertificateName'</param>
+        /// <param name="portNumber">Optional portNumber, otherwise Standard HTTPS 443 will be used</param>
+        /// <param name="azureConnectionString">Optional azureConnectionString, otherwise will be retrieved by ENV 'AzureConnectionString'</param>
         /// <returns>IWebHostBuilder instance</returns>
-        public static IWebHostBuilder ConfigureKestrelSSLFromKeyVault(this IWebHostBuilder webHostBuilder, string keyVaultName = null, string certificateName = null)
+        public static IWebHostBuilder ConfigureKestrelSSLFromKeyVault(this IWebHostBuilder webHostBuilder, string keyVaultName = null, string certificateName = null, int portNumber = 443, string azureConnectionString = null)
         {
-            PrepareKeyVaultClient();
+            var keyVaultClient = PrepareKeyVaultClient(azureConnectionString);
             LoadValues(ref keyVaultName, ref certificateName);
 
             webHostBuilder
                 .ConfigureKestrel((context, options) =>
                 {
-                    const int PortNumber = 5001;
-
                     options.Listen(
-                        new IPEndPoint(IPAddress.Any, PortNumber),
+                        new IPEndPoint(IPAddress.Any, portNumber),
                         listenOptions =>
                         {
                             listenOptions.KestrelServerOptions.AddServerHeader = false;
-                            listenOptions.UseHttps(DownloadCertificateFromKeyVault(keyVaultName, certificateName));
+                            listenOptions.UseHttps(DownloadCertificateFromKeyVault(keyVaultClient, keyVaultName, certificateName));
                         });
                 });
 
@@ -110,10 +136,11 @@ namespace CSE.SecureWebServerHelper
         /// <param name="webHostBuilder">IWebHostBuilder</param>
         /// <param name="keyVaultName">Optional keyVaultName, otherwise will be retrieved by ENV 'KeyVaultName'</param>
         /// <param name="certificateName">Optional certificateName, otherwise will be retrieved by ENV 'CertificateName'</param>
+        /// <param name="azureConnectionString">Optional azureConnectionString, otherwise will be retrieved by ENV 'AzureConnectionString'</param>
         /// <returns>IWebHostBuilder instance</returns>
-        public static IWebHostBuilder ConfigureRootCAFromKeyVault(this IWebHostBuilder webHostBuilder, string keyVaultName = null, string certificateName = null)
+        public static IWebHostBuilder ConfigureRootCAFromKeyVault(this IWebHostBuilder webHostBuilder, string keyVaultName = null, string certificateName = null, string azureConnectionString = null)
         {
-            PrepareKeyVaultClient();
+            var keyVaultClient = PrepareKeyVaultClient(azureConnectionString);
             LoadValues(ref keyVaultName, ref certificateName);
 
             var certificate = keyVaultClient.GetCertificateAsync($"https://{keyVaultName}.vault.azure.net", certificateName).GetAwaiter().GetResult();
@@ -133,25 +160,44 @@ namespace CSE.SecureWebServerHelper
         /// We do not know which extension functions will be used so we have to be prepared for every step. Just creates the keyVaultClient with an Azure Bearer Token
         /// Token is subject to Managed Identity or a Connection String which can be supplied for Dev/Test under ENV 'AzureConnectionString'
         /// </summary>
-        private static void PrepareKeyVaultClient()
+        /// <param name="azureConnectionString">Optional azureConnectionString, otherwise will be retrieved by ENV 'AzureConnectionString'</param>
+        /// <returns>KeyVaultClient</returns>
+        private static KeyVaultClient PrepareKeyVaultClient(string azureConnectionString = null)
         {
-            // Create client only if not yet created by other previous call
-            if (keyVaultClient == null)
+            
+            // If a AzureConnectionString is given as parameter use it, otherwise set it to ENV variable value
+            // REMARK: If ENV is also null, AzureServiceTokenProvider can still get a valid token via Managed Identity, which is the recommended usage pattern
+            if (azureConnectionString == null)
             {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider(envAzureConnectionString);
-                keyVaultClient = new KeyVaultClient(
-                    new KeyVaultClient.AuthenticationCallback(
-                        azureServiceTokenProvider.KeyVaultTokenCallback));
+                azureConnectionString = envAzureConnectionString;
             }
+
+            // Setting Dictonary Key value
+            var key = "ManagedIdentity";
+            if (azureConnectionString != null)
+            {
+                key = azureConnectionString;
+            }
+
+            // Create client only if not yet created by other previous call or connectionString has changed
+            if (!keyVaultClients.ContainsKey(key))
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider(azureConnectionString);
+                keyVaultClients.Add(key, new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
+                                                                azureServiceTokenProvider.KeyVaultTokenCallback)));
+            }
+
+            return keyVaultClients[key];
         }
 
         /// <summary>
         /// Loads a X509 Certificate out of KeyVault including the private Key
         /// </summary>
+        /// <param name="keyVaultClient">The Key Vault Client to be used</param>
         /// <param name="keyVaultName">Optional keyVaultName, otherwise will be retrieved by ENV 'KeyVaultName'</param>
         /// <param name="certificateName">Optional certificateName, otherwise will be retrieved by ENV 'CertificateName'</param>
         /// <returns>X509Certificate2</returns>
-        private static X509Certificate2 DownloadCertificateFromKeyVault(string keyVaultName, string certificateName)
+        private static X509Certificate2 DownloadCertificateFromKeyVault(KeyVaultClient keyVaultClient, string keyVaultName, string certificateName)
         {
             LoadValues(ref keyVaultName, ref certificateName);
 
